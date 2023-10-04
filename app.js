dba41c60538702f913ed16ef196ed5af3bd73dfd
nicolew88 <nicolew88@nycstudents.net>
2023-10-04 13:11:19 -0400

    .

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const weight = Number(prompt("Enter your weight (in kg)"));
-const height = Number(prompt("Enter your height (in m)"));
+const weight = Number(prompt("Enter your weight in kilograms"));
+const height = Number(prompt("Enter your height in meters"));
 
-function cal(x,y) { 
+function cal(x,y){
 console.log(x/(y^2));
-
 }
-cal(weight,height)
+cal(weight,height);
+
+/*function init(){
+const cal = cal();
+healthy(BMI);
+}*/
+
+/* function test(x) {
+    if (x <10 ) {
+    console.log("less");
+} else if (x>15) {
+    console.log("big ole number");
+} else {
+    console.log("something else")
+}
+}
+test (3); */
