62a50ff1a0da6bc971b0a9ea54b03417689808ea
nicolew88 <nicolew88@nycstudents.net>
2023-10-03 23:32:10 -0400

    .

---
+++ app.js
@@ -0,0 +1,8 @@
+const weight = Number(prompt("Enter your weight (in kg)"));
+const height = Number(prompt("Enter your height (in m)"));
+
+function cal(x,y) { 
+console.log(x/(y^2));
+
+}
+cal(weight,height)
