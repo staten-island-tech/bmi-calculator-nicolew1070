0dfec9ede97833953966694d39eb00baba7ed77f
nicolew88 <nicolew88@nycstudents.net>
2023-10-05 13:11:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const name = prompt("Enter your name");
 const weight = Number(prompt("Enter your weight in kilograms"));
 const height = Number(prompt("Enter your height in centimeters"));
-const bmi = getBMI(weight,height); //allows us to use this data for results 
+const bmi = getBMI(weight,height); //allows us to use the results from the function (the bmi we calculated)
 
 function getBMI(weight,height){
-return weight/((height/100)*(height/100));
+return weight/((height/100)*(height/100)); //return will do the math and give us the value
 }
 console.log(getBMI(weight,height)); //without console, no results will be shown
 
@@ -19,4 +19,4 @@ if (bmi < 18.5) {
     console.log("Obese")
 }
 }
-result(); //dont touch
+result(); //displays the result of the function
