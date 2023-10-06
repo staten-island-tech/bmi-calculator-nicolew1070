3e9cf989ff877f718fb47efd87526944540ba542
nicolew88 <nicolew88@nycstudents.net>
2023-10-06 13:12:18 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,6 +1,8 @@
 const date = new Date();
 const year = date.getFullYear();
-const Jane = {
+
+const students = [ 
+    {
     firstName: "jane",
     lastName: "Ladooda",
     graduated: false,
@@ -8,7 +10,36 @@ const Jane = {
     siblings: ["katie", "snake"],
     age: function(){
         return (year - this.DOB);
-    }
-    };
+    },
+    }, 
+    
+    {
+    firstName: "john",
+    lastName: "smith",
+    graduated: false,
+    DOB: 2008,
+    siblings: ["frank", "casey"],
+    age: function(){
+        return (year - this.DOB);
+    },
+    }, 
+    
+    {
+    firstName: "dakota",
+    lastName: "stabeelee",
+    graduated: true,
+    DOB: 2008,
+    siblings: ["freddy", "lily"],
+    age: function(){
+        return (year - this.DOB);
+    },
+    },
+];
 
-console.log(jane.age());
+const numbers = [1,2,3,4,5,6,7,8,9];
+//index starts at 0
+//console.log(numbers[numbers.length - 1]);
+//arrow function ()=> auto returns output, this keyword points global
+/*numbers.forEach((number,index)=> console.log(number,index))*///runs a function for each item in the array 
+const graduated= students.filter((students)=>students.graduated === true);
+console.log(graduated);
