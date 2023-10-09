35c4375acb8cb9ef087aa668e6b40583be84e116
nicolew88 <nicolew88@nycstudents.net>
2023-10-09 19:19:37 -0400

    .

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const bmi = getBMI(weight,height); //allows us to use the results from the funct
 function getBMI(weight,height){
 return weight/((height/100)*(height/100)); //return will do the math and give us the value
 }
-console.log(getBMI(weight,height)); //without console, no results will be shown
+console.log(getBMI(weight,height)); //without console, no results will be shown -> return is useless without console
 
-function result (bmi) { //conditional statements 
+function result (bmi) { //conditional statements using the bmi results from function getBMI
 if (bmi < 18.5) {
     console.log("Underweight");
 } else if (bmi >= 18.5 && bmi <= 24.9) {
