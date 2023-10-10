110111a0be8dbcb7b68e3520c69bae67f096f2bd
nicolew88 <nicolew88@nycstudents.net>
2023-10-10 13:02:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const name = prompt("Enter your name");
 const weight = Number(prompt("Enter your weight in kilograms"));
 const height = Number(prompt("Enter your height in centimeters"));
-const bmi = getBMI(weight,height); //allows us to use the results from the function (the bmi we calculated)
+const bmi = getBMI(weight,height); 
 
 function getBMI(weight,height){
 return weight/((height/100)*(height/100)); //return will do the math and give us the value
